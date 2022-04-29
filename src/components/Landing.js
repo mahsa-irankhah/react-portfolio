f644140a395eaa79c4ddf833a9c971d47fe299d2
Mahsa Irankhah <m.irankhah1995@gmail.com>
2022-04-29 21:33:03 +0430

    initial commit

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+
+const Landing = () => {
+    return (
+        <div>
+            <Navbar/>
+            
+        </div>
+    );
+};
+
+export default Landing;
